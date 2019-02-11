31c3c01c35401d30028f4f82a9b8184da6f62f37
Tommy <tsisak90@gmail.com>
2019-02-11 22:24:13 +0100

    Done for the day

---
+++ HelloWorld2.py
@@ -0,0 +1,26 @@
+__author__ = "CasualTommyst"
+# greeting = "Szia"
+# name = input("Please enter your name here: ")
+# print(greeting + " " + name + "!")
+#
+# splitString = "This string has been\nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t"
+# print(tabbedString)
+
+print("The pet shop owner said \"No, no, He\'s uh,... he\'s resting\"")
+print('The pet shop owner said \"No, no, He\'s uh,... he\'s resting\"')
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print("""The pet shop owner said "No, no, 'e's' uh,... he's resting" """)
+#in this case a space is needed as Py doesn't handle """" so well
+print('''The pet shop owner said "No, no, 'e's' uh,... he's resting"''')
+
+#Printing tabs exercise
+print("Number 1\tThe Larch\nNumber 2\tThe Horse Chestnut")
---
+++ helloworld.py
@@ -0,0 +1,15 @@
+__author__ = "CasualTommyst"
+print("Hello, World!")
+print(1 + 2)
+print(7 * 6)
+print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Bruce"
+print(greeting + name)
+# if we want a space, we can add  that too
+print(greeting + " " + name)
+# exercise just for reference:
+print("My hovercraft is full of eels")
